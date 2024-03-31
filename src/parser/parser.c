3d7c8be8da1d03acb092b63215003b53e1160364
becauselol <gabesytf@gmail.com>
2024-03-31 16:30:34 +0800

    feat: tried to work with fgets

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,15 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LINE_LENGTH 1024;
-FILE *file = fopen("textfile.txt", "r");
-char line[MAX_LINE_LENGTH];
-while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
-{
-printf("Line: %s\n", line);
-}
-fclose(file);
-return 0;
+// defined by the recommended line length for markdown
+#define MAX_LINE_LENGTH 121
 
 // first we need some code to read the file
 char* read_source_code(const char *filename) {
@@ -33,32 +26,84 @@ char* read_source_code(const char *filename) {
     return source_code;
 }
 
+// possible reference for line reading
+// https://stackoverflow.com/questions/29576799/reading-an-unknown-length-line-from-stdin-in-c-with-fgets
+
 int main(int argc, char ** argv) {
   if (argc != 2) {
     printf("No file specified");
     return 1;
   }
   
-  char *source_code = read_source_code(argv[1]);
-
-  if (source_code == NULL) {
-      fprintf(stderr, "Error reading source code from '%s'\n", argv[1]);
-      return 1;
+  // first we read the file
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL) {
+    printf("Error reading file %s", argv[1]);
+    return 1;
   }
-  char *lines = source_code;
-  char delim = '\n';
-  int line_number = 0;
 
-  do {
-    printf("line: %d\n", line_number);
-    printf("%s\n", lines);
-    lines++;
-    lines = strstr()
+  // we make an easy assumption that the MAX_LINE_LENGTH is 1024
+  char buffer[MAX_LINE_LENGTH];
+  int buffer_length;
+
+  char *new_line_char;
+  char *line = (char *) calloc(MAX_LINE_LENGTH, sizeof(char));
+  char *ptr = line;
+  int line_size = MAX_LINE_LENGTH;
+  int line_length = 0;
+  int line_number = 0;
+  int is_empty_line = 0;
+
+  while (fgets(buffer, MAX_LINE_LENGTH, file) != NULL) {
+    // the idea is we have a buffer
+    printf("Buffer: %s\n", buffer);
+
+    // we want to copy whatever in the buffer over to the line
+    buffer_length = strlen(buffer);
+
+    line_length += buffer_length;
+    if (line_length > line_size) {
+
+      // we need to double the size;
+      line_size *= 2;
+      line = (char *) realloc(line, line_size * sizeof(char));
+    }
+    
+    // then we copy safely
+    strncpy(ptr, buffer, buffer_length);
+
+    // we move the pointer along so we can copy correctly next
+    ptr += buffer_length;
+    // we want to check if there is a new line in the 
+    new_line_char = strstr(buffer, "\n");
+
+    // if there is no new line, we keep reading
+    if (new_line_char == NULL) {
+      // we continue reading
+      continue;
+    } 
+    
+    // shift the pointer back slightly for the '\n'
+    ptr--;
+    // replace the pointer with '\0' so we don't deal with '\n'
+    *ptr = '\0';
+    is_empty_line = (buffer[0] == '\n') ? 1 : 0;
+    printf("Line number: %d\n", line_number);
+    printf("Copied Line: %s\n", line);
+    printf("is_empty_line %d\n", is_empty_line);
+    // Now we need to parse and figure out what the node is
+
+    // now we want to empty line
+    memset(line, '\0', line_size);
+    // reset the pointer to the start
+    ptr = line;
+    // and reset the line_length
+    line_length = 0;
+    // we increment line number as well
     line_number++;
   }
-  while ((lines =)) {
 
-  }
-
+  free(line);
+  fclose(file);
   return 0;
 }
