0f7e6ffb6f88bdd3c90d3b24c50559bf42baa570
becauselol <gabesytf@gmail.com>
2024-04-09 13:20:39 +0800

    feat: implement href function in paragraph

--- src/parser/features/paragraph.c
+++ src/parser/features/paragraph.c
@@ -57,16 +57,17 @@ void process_paragraph_data(md_node *node) {
   int i, start, end;
   md_node *new_child_node;
   char *temp_text;
+  char *matching;
+  char *close_link;
   int temp_length;
   start = 0;
   for (i = 0; i < text_length; i++) {
     switch (text_data[i]) {
     case '*':
       if (i > 0 && text_data[i - 1] == '\\') {
-        continue;
+        break;
       }
 
-      printf("before: %c, after: %c\n", text_data[i - 1], text_data[i + 1]);
       if ((i == 0) || ((i > 0 && text_data[i - 1] == ' ') &&
                        (i < text_length - 1 && text_data[i + 1] != ' '))) {
 
@@ -100,7 +101,6 @@ void process_paragraph_data(md_node *node) {
         continue;
       }
 
-      printf("before: %c, after: %c\n", text_data[i - 1], text_data[i + 1]);
       if ((i == 0) || ((i > 0 && text_data[i - 1] == ' ') &&
                        (i < text_length - 1 && text_data[i + 1] != ' '))) {
 
@@ -128,6 +128,63 @@ void process_paragraph_data(md_node *node) {
         set_text_data(node->last_child, temp_text, temp_length);
         start = i + 1;
       }
+      break;
+    case '[':
+      // we now want to find the matching
+      matching = strstr(&text_data[i], "]");
+      if (matching == NULL) {
+        printf("no matching '] found\n'");
+        break;
+      }
+
+      // if we found, we want the next one to be "("
+      if (*(matching + 1) != '(') {
+        printf("No start of '(' found\n");
+        break;
+      }
+
+      if ((close_link = strstr(matching, ")")) == NULL) {
+        printf("No matching ')' found\n");
+        break;
+      }
+
+      // now all criteria fulfilled we want to copy the data
+
+      end = i - 1;
+      temp_length = end - start + 1;
+      temp_text = (char *)calloc(temp_length + 1, sizeof(char));
+      strncpy(temp_text, &text_data[start], temp_length);
+      temp_text[temp_length] = '\0';
+      set_text_data(node, temp_text, temp_length);
+
+      // we start copying URL data
+      new_child_node = create_empty_md_node(NODE_LINK);
+      append_to_root(node, new_child_node);
+
+      // we copy the text to replace the link
+      temp_length = matching - &text_data[i] - 1;
+      temp_text = (char *)calloc(temp_length + 1, sizeof(char));
+      strncpy(temp_text, &text_data[i] + 1, temp_length);
+      temp_text[temp_length] = '\0';
+      printf("%d, %s\n", temp_length, temp_text);
+      set_text_data(node->last_child, temp_text, temp_length);
+
+      i += (temp_length + 2);
+      // now we want to set the URL
+      temp_length = close_link - (matching + 1) - 1;
+      temp_text = (char *)calloc(temp_length + 1, sizeof(char));
+      strncpy(temp_text, matching + 2, temp_length);
+      temp_text[temp_length] = '\0';
+      new_child_node->url_length = temp_length;
+      new_child_node->url =
+          (char *)calloc(new_child_node->url_length + 1, sizeof(char));
+      printf("%d, %s\n", temp_length, temp_text);
+      strncpy(new_child_node->url, temp_text, temp_length + 1);
+
+      // we need to set i to move forward
+      i += (temp_length + 2);
+      start = i;
+
       break;
     default:
       break;
