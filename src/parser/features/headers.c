e5b148bd1388dcb129441ba63971fbbc781fed0c
becauselol <gabesytf@gmail.com>
2024-04-10 03:15:33 +0800

    fix: swap header identification for regex

--- src/parser/features/headers.c
+++ src/parser/features/headers.c
@@ -1,40 +1,20 @@
 #include "headers.h"
+#include <regex.h>
 #include <stdio.h>
 #include <string.h>
 
 int is_header(const char *line, int line_length) {
-  // // check if its a header,
-  // regex_t header_regex;
-  // int value;
-  // // Creation of regEx
-  // // we are opinionated and need the space
-  // value = regcomp( &header_regex, "^(#{1,6} .+)", 0);
-  // if (value != 0) {
-  //   printf("regex didn't compile");
-  // }
-  // value = regexec( &header_regex, line,
-  //                   0, NULL, 0);
-  // return value == 0;
-
-  if (line == NULL) {
-    return 0;
+  // check if its a header,
+  regex_t header_regex;
+  int value;
+  // Creation of regEx
+  // we are opinionated and need the space
+  value = regcomp(&header_regex, "^(#{1,6} .+)", REG_EXTENDED);
+  if (value != 0) {
+    printf("regex didn't compile");
   }
-  if (line[0] != '#') {
-    return 0;
-  }
-  int have_hex = 1;
-  int counter = 0;
-  for (int i = 0; i < line_length; i++) {
-    if (line[i] == '#') {
-      counter++;
-      continue;
-    } else if (line[i] == ' ') {
-      break;
-    } else {
-      have_hex = 0;
-    }
-  }
-  return counter <= 6 ? have_hex : 0;
+  value = regexec(&header_regex, line, 0, NULL, 0);
+  return value == 0;
 }
 
 int get_header_level(const char *line, int line_length) {
